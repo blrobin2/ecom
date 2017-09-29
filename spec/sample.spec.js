5bf126ffd4c22d256eb6df89831abe2a54ae1fd1
Bennie <brobinson88@gmail.com>
2017-09-29 10:29:57 -0400

    Add console spy for funsies

--- spec/sample.spec.js
+++ spec/sample.spec.js
@@ -2,6 +2,8 @@ import test from '../public/index'
 
 describe('Fake test', () => {
   it('demonstrates test suite is working', () => {
+    console.log = jasmine.createSpy('log')
     expect(test()).toBeTruthy()
+    expect(console.log).toHaveBeenCalledWith('I was called')
   })
 })
